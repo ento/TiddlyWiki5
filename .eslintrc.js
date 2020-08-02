d322f31cf28a850152ea96996f449061c13f43fe
ento <ento+github@i.pearlwaffles.xyz>
2020-08-01 21:31:57 -0800

    eslint: Ignore current violations

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
 	"extends": [
 		"eslint:recommended",
 		// TiddlyWiki-specific rules
-		"./eslint/base.config.js"
+		"./eslint/base.config.js",
+		// List of ignored files while we work on fixing all code style inconsistencies
+		"./eslint/todo.config.js"
 	]
 };
