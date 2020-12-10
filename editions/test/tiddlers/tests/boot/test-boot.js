48c7d70635dbe8b79c5999d3de4b54f5a735bfa9
ento <ento+github@i.pearlwaffles.xyz>
2020-12-10 11:07:20 -0800

    Add tests for boot module's execStartup function

---
+++ editions/test/tiddlers/tests/boot/test-boot.js
@@ -0,0 +1,112 @@
+/*\
+title: boot/test-widget.js
+type: application/javascript
+tags: [[$:/tags/test-spec]]
+
+Tests the boot module.
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: true */
+	"use strict";
+
+// The boot module has global side-effects when executed in browser,
+// which makes it hard to test. We contend with only testing in Node.
+if($tw.node) {
+	describe("boot module",function() {
+		var path = require("path");
+		var boot = require(path.join($tw.boot.bootPath,"boot.js")).TiddlyWiki;
+		function createTw() {
+			var tw = boot();
+			// Run only the essential initialization code from $tw.boot.initStartup().
+			tw.wiki = new tw.Wiki();
+			return tw;
+		}
+		describe("execStartup()",function() {
+			it("executes simple startup modules",function(done) {
+				var $tw = createTw();
+				var log = [];
+				$tw.modules.define("start1","startup",{
+					startup: function() {
+						log.push("start1");
+					},
+				});
+				$tw.modules.define("start2","startup",{
+					startup: function() {
+						log.push("start2");
+					},
+				});
+				$tw.boot.execStartup({callback: function() {
+					expect(log).toEqual(["start1","start2"]);
+					done();
+				}});
+			});
+			it("skips startup modules that do not match the current platform",function(done) {
+				var $tw = createTw();
+				var log = [];
+				$tw.modules.define("start1","startup",{
+					platforms: ["browser"],
+					startup: function() {
+						log.push("start1");
+					},
+				});
+				$tw.boot.execStartup({callback: function() {
+					expect(log).toEqual([]);
+					done();
+				}});
+			});
+			it("executes startup modules ordered by 'after'",function(done) {
+				var $tw = createTw();
+				var log = [];
+				$tw.modules.define("after2","startup",{
+					after: ["start2"],
+					startup: function() {
+						log.push("after2");
+					},
+				});
+				$tw.modules.define("start2","startup",{
+					name: "start2",
+					startup: function() {
+						log.push("start2");
+					},
+				});
+				$tw.boot.execStartup({callback: function() {
+					expect(log).toEqual(["start2","after2"]);
+					done();
+				}});
+			});
+			it("executes async startup modules ordered by 'after'",function(done) {
+				var $tw = createTw();
+				var log = [];
+				$tw.modules.define("after2","startup",{
+					after: ["start2"],
+					synchronous: false,
+					startup: function(callback) {
+						setTimeout(function(){
+							log.push("after2");
+							callback();
+						}, 0);
+					},
+				});
+				$tw.modules.define("start2","startup",{
+					name: "start2",
+					synchronous: false,
+					startup: function(callback) {
+						setTimeout(function(){
+							log.push("start2");
+							callback();
+						}, 0);
+					},
+				});
+				$tw.boot.execStartup({callback: function() {
+					expect(log).toEqual(["start2","after2"]);
+					done();
+				}});
+			});
+		});
+	});
+}
+
+})();
